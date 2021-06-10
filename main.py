402ae7dab9e1f19e863accc7160a740d9206b6cd
gavin spearhead <gavin.spearhead+github@gmail.com>
2021-06-11 00:02:41 +0200

    added display

--- main.py
+++ main.py
@@ -48,6 +48,113 @@ Press Ctrl+C to exit!
 """)
 
 DEBUG = os.getenv('DEBUG', 'false') == 'true'
+def calculate_y_pos(x, centre):
+    """Calculates the y-coordinate on a parabolic curve, given x."""
+    centre = 80
+    y = 1 / centre * (x - centre) ** 2
+
+    return int(y)
+
+
+def circle_coordinates(x, y, radius):
+    """Calculates the bounds of a circle, given centre and radius."""
+
+    x1 = x - radius  # Left
+    x2 = x + radius  # Right
+    y1 = y - radius  # Bottom
+    y2 = y + radius  # Top
+
+    return (x1, y1, x2, y2)
+
+
+
+def map_colour(x, centre, start_hue, end_hue, day):
+    """Given an x coordinate and a centre point, a start and end hue (in degrees),
+       and a Boolean for day or night (day is True, night False), calculate a colour
+       hue representing the 'colour' of that time of day."""
+
+    start_hue = start_hue / 360  # Rescale to between 0 and 1
+    end_hue = end_hue / 360
+
+    sat = 1.0
+
+    # Dim the brightness as you move from the centre to the edges
+    val = 1 - (abs(centre - x) / (2 * centre))
+
+    # Ramp up towards centre, then back down
+    if x > centre:
+        x = (2 * centre) - x
+
+    # Calculate the hue
+    hue = start_hue + ((x / centre) * (end_hue - start_hue))
+
+    # At night, move towards purple/blue hues and reverse dimming
+    if not day:
+        hue = 1 - hue
+        val = 1 - val
+
+    r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(hue, sat, val)]
+
+    return (r, g, b)
+
+
+def x_from_sun_moon_time(progress, period, x_range):
+    """Recalculate/rescale an amount of progress through a time period."""
+
+    x = int((progress / period) * x_range)
+
+    return x
+
+
+def sun_moon_time(city_name, time_zone):
+    """Calculate the progress through the current sun/moon period (i.e day or
+       night) from the last sunrise or sunset, given a datetime object 't'."""
+
+    city = lookup(city_name, database())
+
+    # Datetime objects for yesterday, today, tomorrow
+    utc = pytz.utc
+    utc_dt = datetime.now(tz=utc)
+    local_dt = utc_dt.astimezone(pytz.timezone(time_zone))
+    today = local_dt.date()
+    yesterday = today - timedelta(1)
+    tomorrow = today + timedelta(1)
+
+    # Sun objects for yesterday, today, tomorrow
+    sun_yesterday = sun(city.observer, date=yesterday)
+    sun_today = sun(city.observer, date=today)
+    sun_tomorrow = sun(city.observer, date=tomorrow)
+
+    # Work out sunset yesterday, sunrise/sunset today, and sunrise tomorrow
+    sunset_yesterday = sun_yesterday["sunset"]
+    sunrise_today = sun_today["sunrise"]
+    sunset_today = sun_today["sunset"]
+    sunrise_tomorrow = sun_tomorrow["sunrise"]
+
+    # Work out lengths of day or night period and progress through period
+    if sunrise_today < local_dt < sunset_today:
+        day = True
+        period = sunset_today - sunrise_today
+        # mid = sunrise_today + (period / 2)
+        progress = local_dt - sunrise_today
+
+    elif local_dt > sunset_today:
+        day = False
+        period = sunrise_tomorrow - sunset_today
+        # mid = sunset_today + (period / 2)
+        progress = local_dt - sunset_today
+
+    else:
+        day = False
+        period = sunrise_today - sunset_yesterday
+        # mid = sunset_yesterday + (period / 2)
+        progress = local_dt - sunset_yesterday
+
+    # Convert time deltas to seconds
+    progress = progress.total_seconds()
+    period = period.total_seconds()
+
+    return progress, period, day, local_dt
 
 
 def overlay_text(img, position, text, font, align_right=False, rectangle=False):
@@ -71,6 +178,42 @@ def overlay_text(img, position, text, font, align_right=False, rectangle=False):
     else:
         draw.text(position, text, font=font, fill=(255, 255, 255))
     return img
+
+
+def draw_background(progress, period, day):
+    """Given an amount of progress through the day or night, draw the
+       background colour and overlay a blurred sun/moon."""
+
+    # x-coordinate for sun/moon
+    x = x_from_sun_moon_time(progress, period, WIDTH)
+
+    # If it's day, then move right to left
+    if day:
+        x = WIDTH - x
+
+    # Calculate position on sun/moon's curve
+    centre = WIDTH / 2
+    y = calculate_y_pos(x, centre)
+
+    # Background colour
+    background = map_colour(x, 80, mid_hue, day_hue, day)
+
+    # New image for background colour
+    img = Image.new('RGBA', (WIDTH, HEIGHT), color=background)
+    # draw = ImageDraw.Draw(img)
+
+    # New image for sun/moon overlay
+    overlay = Image.new('RGBA', (WIDTH, HEIGHT), color=(0, 0, 0, 0))
+    overlay_draw = ImageDraw.Draw(overlay)
+
+    # Draw the sun/moon
+    circle = circle_coordinates(x, y, sun_radius)
+    overlay_draw.ellipse(circle, fill=(200, 200, 50, opacity))
+
+    # Overlay the sun/moon on the background as an alpha matte
+    composite = Image.alpha_composite(img, overlay).filter(ImageFilter.GaussianBlur(radius=blur))
+
+    return composite
 
 
 class EnviroCollector:
@@ -267,7 +410,6 @@ if __name__ == '__main__':
     font_sm = ImageFont.truetype(UserFont, 12)
     font_lg = ImageFont.truetype(UserFont, 14)
 
-
     disp.begin()
     margin = 3
 
@@ -293,8 +435,6 @@ if __name__ == '__main__':
     while True:
         ec.update_all()
 
-
-
         now2 = datetime.datetime.now(pytz.UTC) - now1
         remaining_time = args.timeout - (now2.seconds + (now2.microseconds / 1000000))
         if remaining_time <= 0:
@@ -306,6 +446,10 @@ if __name__ == '__main__':
                 temp_string = "{:.0f}°C".format(data['temperature'])
                 humidity_string = "{:.0f}%".format(data['humidity'])
                 pressure_string = "{}".format(int(data['pressure']))
+                background = draw_background(progress, period, day)
+
+                mg = overlay_text(background, (0 + margin, 0 + margin), time_string, font_lg)
+                img = overlay_text(img, (WIDTH - margin, 0 + margin), date_string, font_lg, align_right=True)
                 img = overlay_text(img, (68, 18), temp_string, font_lg, align_right=True)
                 img = overlay_text(img, (68, 48), humidity_string, font_lg, align_right=True)
                 img = overlay_text(img, (WIDTH - margin, 48), pressure_string, font_lg, align_right=True)
